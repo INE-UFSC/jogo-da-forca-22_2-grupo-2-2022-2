c1c918ff598f681194e449cf27878e8b20eef954
Maykon <maykon.marcos.junior@grad.ufsc.br>
2022-09-02 11:37:35 -0300

    atualização

--- Jogo_Da_Forca.py
+++ Jogo_Da_Forca.py
@@ -93,15 +93,15 @@ forca = [
     '''
     ]
 palavra = input()
+status = list(palavra)
 c = len(palavra)
 chance = 0
 saida = ['_']*c
 # estabelecendo o critério para mais de uma palavra
-if ' ' in palavra:
-    k = palavra.count(' ')
-    for i in range(k): 
-        l = palavra.index(' ', i)
-        saida[l] = ' '
+while ' ' in status:
+    l = status.index(' ')
+    saida[l] = ' '
+    status[l] = '*'
 # transformando em string e printando o template vazio 
 r = ''.join(saida)
 print(forca[0])
@@ -110,14 +110,14 @@ print(r)
 while (r != palavra) and (chance < 8):
     letra = input()
     if letra in palavra:
-        k = palavra.count(letra)
         # após verificar quantas vezes a letra aparece, substituir '_' pela letra
-        for i in range(k):
-            l = palavra.index(letra, i)
+        while letra in status:
+            l = status.index(letra)
             saida[l] = letra
-            r = ''.join(saida)
+            status[l] = '*'
     else: 
         chance += 1
     # atualizar o template com a última tentativa
     print(forca[chance])
+    r = ''.join(saida)
     if chance < 8: print(r)
